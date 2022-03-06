f5c50a59e5597ce2870f24535f858856125933c0
Patrick Kenneally <pkenneally93@gmail.com>
2022-03-06 16:32:39 +0000

    Updated import of ipfs-core

--- src/components/IPFSNode/IPFSNode.tsx
+++ src/components/IPFSNode/IPFSNode.tsx
@@ -1,37 +1,35 @@
-import { useEffect } from "react";
-// import * as IPFS from 'ipfs-core'
+import { useState } from "react";
 import './IPFSNode.css'
+
+declare global {
+    interface Window { Ipfs: any; }
+}
 
 const IPFSNode = (props:any) => {
 
-  // let nodeSet = false;
+  const [nodeSet, setNodeSet] = useState(false);
 
-  // IPFS.create().then( async (node:any) => {
-  //   await props.setMyNode(node);
-  //   const results = await props.myNode.add('=^.^= meow meow')
-  //   nodeSet = true;
-  //   console.log({ myNode:props.myNode, results })
-  // }).catch(async (e:any) => {
-  //   console.log({ e })
-  //   if(nodeSet){
-  //     const results = await props.myNode.add('=^.^= meow meow')
-  //     console.log({ myNode: props.myNode, results })
-  //   }
-  // });
-
-  // const cid = results[0].hash
-  // console.log('CID created via ipfs.add:', cid)
-  // const data = await node.cat(cid)
-  // console.log('Data read back via ipfs.cat:', new TextDecoder().decode(data))
-
-  useEffect(() => {
-    const script = document.createElement("script");
-
-    script.src = "https://cdn.jsdelivr.net/npm/ipfs/dist/index.min.js";
-    script.async = true;
-    console.log(`i made it`);
-    document.body.appendChild(script);
-  });
+  //const cid = results[0].hash
+  //console.log('CID created via ipfs.add:', cid)
+  //const data = await node.cat(cid)
+  //console.log('Data read back via ipfs.cat:', new TextDecoder().decode(data))
+  if (window && window.Ipfs) {
+    window.Ipfs.create({
+      repo: String(Math.random() + Date.now()),
+      init: { algorithm: 'Ed25519' }
+    }).then( async (node:any) => {
+      await props.setMyNode(node);
+      const results = await props.myNode.add('=^.^= meow meow')
+      setNodeSet(true)
+      console.log({ myNode:props.myNode, results })
+    }).catch(async (e:any) => {
+      console.log({ e })
+      if(nodeSet){
+      const results = await props.myNode.add('=^.^= meow meow')
+      console.log({ myNode: props.myNode, results })
+      }
+    });
+  }
 
   return (
     <>
